a4f4e3871f7c6d4fb9ec6cb57e4204f2047928fb
Lolmaia <131406007+Lolmaia@users.noreply.github.com>
2023-05-02 14:18:13 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,6 +1,6 @@
 function Enviar() {
 
-    var nome = document.getElementById("name").value;
+    var nome = document.getElementById("name").value;/* Armazena na variável nome o dado digitado no campo name do HTML*/
     var mail = document.getElementById("mail").value;
     var cmbquem = document.getElementById("cmbquem").value;
     var msg = document.getElementById("msg").value;
@@ -8,9 +8,14 @@ function Enviar() {
 
 
     if (nome != "" && mail!= "" && cmbquem != "" && msg != "") {
+        var obj = { 'nome': nome, 'e-mail': mail, 'id':cmbquem, 'sugestao':msg};
+        localStorage.setItem('dadosform', JSON.stringify(obj));/* Permite salvar pares de chave e valores no navegador do cliente.*/
         alert('Obrigado sr(a) ' + nome + ' a sua sugestão foi enviada com sucesso!');
     }else{
         alert("Campo obrigatório não preenchido! Favor conferir!");
     }
+     var retrievedObject = localStorage.getItem('dadosform');/* Permite recuperar o valor armazenado a partir de sua chave.*/
+    console.log(retrievedObject);/* Teste de armazenamento*/
+    
 
 }
